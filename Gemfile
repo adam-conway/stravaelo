7329dc2c9c7d8eaf221880145e87ad04253d850c
adam-conway <adam.n.conway@gmail.com>
2018-05-21 10:24:07 -0600

    Added test for visiting home page

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'uglifier', '>= 1.3.0'
 group :development, :test do
   gem 'pry'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'capybara'
 end
 
 group :development do
--- spec/features/visitor/visitor_goes_to_home_page_spec.rb
+++ spec/features/visitor/visitor_goes_to_home_page_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 describe "Visitor" do
   describe "visits the home page" do
     scenario "Sees all expected titles and links on home page" do
+      visit '/'
+      # When I visit 'www.stravaelo.com'
 
+      expect(current_path).to eq('/')
+      expect(page).to have_content('straVaELO')
+      expect(page).to have_css('.nav')
+      within('.nav') do
+        expect(page).to have_content('Log In')
+      end
+
+      expect(page).to have_link('Log in with Strava')
     end
   end
 end
