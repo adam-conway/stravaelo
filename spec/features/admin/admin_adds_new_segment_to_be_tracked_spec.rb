6c88f11dfac1566fd38f765ec948cca6fbe3bee0
adam-conway <adam.n.conway@gmail.com>
2018-05-23 18:20:50 -0600

    Added test for adding segment for tracking

--- spec/features/admin/admin_adds_new_segment_to_be_tracked_spec.rb
+++ spec/features/admin/admin_adds_new_segment_to_be_tracked_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 describe 'Admin' do
   scenario 'adds a new segment to be tracked' do
-    VCR.use_cassette("Adding segment") do
+    VCR.use_cassette("Adding-segment") do
       visit new_segment_path
       expect(current_path).to eq('/segments/new')
 
-      click_link 'Log In With Strava'
-      expect(page).to have_link('Adam Conway')
+      fill_in :id, with: 229781
+      click_on "Add Segment"
+
+      expect(current_path).to eq('/segments')
+      expect(page).to have_content('Hawk Hill was successfully added')
+      expect(page).to have_link('Hawk Hill')
     end
   end
 end
