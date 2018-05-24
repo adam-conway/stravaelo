f903cd5b19f4f83ba9e49b7fffb1dda5cdfdec1b
adam-conway <adam.n.conway@gmail.com>
2018-05-24 12:15:22 -0600

    Added logic for using SegmentQuery and test for it

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -1,7 +1,16 @@
 class SegmentsController < ApplicationController
+  def index
+  end
+
   def new
   end
 
   def create
+    if Segment.find_by_id(params[:id])
+      flash[:error] = "This segment has already been added"
+    else
+      segment_hash = SegmentQuery.new(params[:id]).raw_data
+      Segment.create(segment_hash)
+    end
   end
 end
