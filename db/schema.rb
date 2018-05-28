2d34596073d0a550d3692300744ce1b991bde9af
adam-conway <adam.n.conway@gmail.com>
2018-05-28 10:23:09 -0600

    Added tournaments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_225356) do
+ActiveRecord::Schema.define(version: 2018_05_28_162231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2018_05_23_225356) do
     t.float "total_elevation_gain"
     t.integer "athlete_attempts"
     t.integer "unique_athletes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
