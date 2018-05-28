8507b2f45b8cfd7016b1e8569eb10b14f04ca300
adam-conway <adam.n.conway@gmail.com>
2018-05-28 09:04:29 -0600

    Added test for admin only segment delete button

--- spec/features/visitor/non_admin_cannot_add_or_delete_segment_spec.rb
+++ spec/features/visitor/non_admin_cannot_add_or_delete_segment_spec.rb
@@ -5,4 +5,16 @@ describe 'User' do
     visit segments_path
     expect(page).to_not have_selector('input')
   end
+
+  scenario 'cant add a new segment' do
+    segment = create(:segment)
+    visit segments_path
+    expect(page).to have_content(segment.name)
+
+    within("tr.segment-#{segment.id}") do
+      expect(page).to_not have_link('Delete')
+    end
+
+    expect(page).to have_content(segment.name)
+  end
 end
