5cc726193301e27d60c419aecd6826cc73b8d9bb
adam-conway <adam.n.conway@gmail.com>
2018-05-28 09:02:26 -0600

    Added protection for creating segments for admin only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :current_admin?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
   end
 end
