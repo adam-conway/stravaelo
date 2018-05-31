76d72d139aebece39baa21662bd09179fb049b62
adam-conway <adam.n.conway@gmail.com>
2018-05-31 14:37:10 -0600

    Finished api call for displaying pr times for a segment

--- app/services/user_ranking_service.rb
+++ app/services/user_ranking_service.rb
@@ -13,7 +13,6 @@ private
 
   def response
     Faraday.get "https://www.strava.com/api/v3/segments/#{segment_id}/all_efforts?access_token=#{token}"
-    # https://www.strava.com/api/v3/segments/5816161/all_efforts?access_token=489ed4cead0b0624997d0c026200a6912e8662b5
   end
 
 
