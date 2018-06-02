da136530f14e67ddb615435a73a1a0f88737e0c6
adam-conway <adam.n.conway@gmail.com>
2018-06-02 11:54:54 -0600

    Fixing user sees tournament show page test

--- spec/features/user/user_invites_people_to_app_spec.rb
+++ spec/features/user/user_invites_people_to_app_spec.rb
@@ -22,30 +22,6 @@ describe 'User' do
         expect(current_path).to eq(tournament_path(tournament1))
         expect(page).to have_content(user2.name)
       end
-      # VCR.use_cassette("Invite-to-tournament") do
-      #   user1 = create(:user, token: ENV['strava_my_token'])
-      #   tournament1 = create(:tournament)
-      #   segment1 = create(:segment, id: 1470688)
-      #   segment2 = create(:segment, id: 5816161)
-      #   create(:user_tournament, user: user1, tournament: tournament1)
-      #   create(:tournament_segment, tournament: tournament1, segment: segment1)
-      #   create(:tournament_segment, tournament: tournament1, segment: segment2)
-      #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-      #   visit tournament_path(tournament1)
-      #
-      #   fill_in :invite, with: "adam.n.conway@gmail.com"
-      #   click_on "Invite to tournament"
-      #
-      #   email = ActionMailer::Base.deliveries.last
-      #
-      #   expect(email.from).to eq(['no-reply@battleshift.com'])
-      #   expect(email.to).to eq([user.email])
-      #   expect(email.subject).to eq('Welcome to Battleshift!')
-      #   expect(email.html_part.body.to_s).to have_content('Visit here to activate your account.')
-      #   expect(email.html_part.body.to_s).to have_content('Visit here to activate your account.')
-      #   expect(email.text_part.body.to_s).to have_content("Your API key: #{user.api_key}")
-      #   expect(email.html_part.body.to_s).to have_content("Your API key: #{user.api_key}")
-      # end
     end
   end
 end
