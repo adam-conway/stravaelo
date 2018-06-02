ac9cc81937e584311e96b14fe5222746aa3c0903
adam-conway <adam.n.conway@gmail.com>
2018-06-02 14:30:17 -0600

    Added protection against inviting users to tournament that are already part of it

--- app/controllers/user_tournaments_controller.rb
+++ app/controllers/user_tournaments_controller.rb
@@ -1,8 +1,10 @@
 class UserTournamentsController < ApplicationController
   def create
     tournament = Tournament.find(params[:tournament_id])
-    if User.find_by(email: params[:invite_email])
+    if User.find_by(email: params[:invite_email]) && !tournament.users.pluck(:email).include?(params[:invite_email])
       tournament.user_tournaments.create(user_id: User.find_by(email: params[:invite_email]).id)
+    elsif tournament.users.pluck(:email).include?(params[:invite_email])
+      flash[:error] = "That user is already part of this tournament!"
     else
       UserMailer.with(challengee: params[:invite_email], challenger: tournament.users.first).challenge_email.deliver_now
     end
--- spec/features/user/user_invites_people_to_app_spec.rb
+++ spec/features/user/user_invites_people_to_app_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User' do
   context 'Add a friend to tournament' do
-    scenario 'who is part of stavelo already' do
+    scenario 'who is part of stavelo already but not part of tournament' do
       VCR.use_cassette("Invite-to-tournament") do
         user1 = create(:user, token: ENV['strava_my_token'])
         user2 = create(:user, email: "test@test.com", token: 567)
@@ -21,6 +21,27 @@ describe 'User' do
         expect(ActionMailer::Base.deliveries.last).to eq(nil)
         expect(current_path).to eq(tournament_path(tournament1))
         expect(page).to have_content(user2.name)
+        expect(page).to_not have_content("That user is already part of this tournament!")
+      end
+    end
+    scenario 'who is part of stavelo already and already part of tournament' do
+      VCR.use_cassette("Already-part-of-tournament") do
+        user1 = create(:user, token: ENV['strava_my_token'])
+        tournament1 = create(:tournament)
+        segment1 = create(:segment, id: 1470688)
+        segment2 = create(:segment, id: 5816161)
+        create(:user_tournament, user: user1, tournament: tournament1)
+        create(:tournament_segment, tournament: tournament1, segment: segment1)
+        create(:tournament_segment, tournament: tournament1, segment: segment2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+        visit tournament_path(tournament1)
+
+        fill_in :invite_email, with: user1.email
+        click_on "Invite to tournament"
+
+        expect(ActionMailer::Base.deliveries.last).to eq(nil)
+        expect(current_path).to eq(tournament_path(tournament1))
+        expect(page).to have_content("That user is already part of this tournament!")
       end
     end
     scenario 'who isnt part of stravelo' do
