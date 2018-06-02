37b8f12d6d9af4a6842beac6f7657237f41adef7
adam-conway <adam.n.conway@gmail.com>
2018-06-02 14:51:47 -0600

    Added a migration to give a user an active or pending status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   enum role: [ "default", "admin" ]
+  enum status: [ "active", "pending" ]
+  
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
