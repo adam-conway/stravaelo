7180a5530b567c392261a012305431754ac7f1b9
adam-conway <adam.n.conway@gmail.com>
2018-06-03 12:03:29 -0600

    Removed transactional callbacks from application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Stravaelo
       enable_starttls_auto: true
     }
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
