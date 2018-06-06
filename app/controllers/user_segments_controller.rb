5c917d41d171e3241094cd1558d290e9d5af2f25
adam-conway <adam.n.conway@gmail.com>
2018-06-05 18:29:39 -0600

    Added flash message to updating segment prs

--- app/controllers/user_segments_controller.rb
+++ app/controllers/user_segments_controller.rb
@@ -1,5 +1,6 @@
 class UserSegmentsController < ApplicationController
   def update
     TournamentUpdater.new(Tournament.find(params[:id])).update
+    flash[:success] = 'Updated PR times!'
   end
 end
