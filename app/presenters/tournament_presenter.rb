ba14720ea0036b0e3065fa127b4f28906b1098c1
adam-conway <adam.n.conway@gmail.com>
2018-06-06 16:23:05 -0600

    Added a ranking function

--- app/presenters/tournament_presenter.rb
+++ app/presenters/tournament_presenter.rb
@@ -35,4 +35,33 @@ class TournamentPresenter
   def pending_users
     tournament.pending_users
   end
+
+  def user_performance(user, segment)
+    performance_percentage(user, segment)
+  end
+
+  def total_user_performance_score(user)
+    tournament.segments.map do |segment|
+      performance_percentage(user, segment)
+    end.sum.round(2)
+  end
+
+  def rank
+    active_users.sort_by do |user|
+      -total_user_performance_score(user)
+    end
+  end
+
+  private
+
+
+    def performance_percentage(user, segment)
+      user_time = UserSegment.where(segment_id: segment).where(user_id: user).first&.pr
+      segment_pr = Segment.find(segment.id).kom_time
+      if user_time == 0 || user_time.nil?
+        0
+      else
+        (segment_pr / user_time.to_f).round(2)
+      end
+    end
 end
