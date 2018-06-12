6b7724f4b09eace4d1213f826e94e4ff7eca7630
adam-conway <adam.n.conway@gmail.com>
2018-06-12 13:32:36 -0700

    Added a seed file for future testing

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,4 +1,7 @@
 class Segment < ApplicationRecord
   has_many :tournament_segments
   has_many :tournaments, through: :tournament_segments
+
+  has_many :user_segments
+  has_many :users, through: :user_segments
 end
